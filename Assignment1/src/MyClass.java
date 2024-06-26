0cf8f8b4dc83e3e930f34ac570b547b413fdfd3a
Monica B <monicab3002@gmail.com>
2024-06-25 22:28:20 -0700

    Starting A1 code. Added default class main function

---
+++ Assignment1/src/MyClass.java
@@ -0,0 +1,20 @@
+public class MyClass {
+    public static void main(String[] args) {
+
+        char current_location = args[0].charAt(0);
+        // state: true -> clean, false -> not clean
+        boolean A_status = Boolean.parseBoolean(args[1]);
+        boolean B_status = Boolean.parseBoolean(args[2]);
+        boolean C_status = Boolean.parseBoolean(args[3]);
+        boolean D_status = Boolean.parseBoolean(args[4]);
+        System.out.println("Current Location = " + current_location + "\n" +
+                "Square A status = " + A_status + "\n" +
+                "Square B status = " + B_status + "\n" +
+                "Square C status = " + C_status + "\n" +
+                "Square D status = " + D_status + "\n");
+
+        // WRITE A1 FUNCTION HERE
+
+        System.out.println("\nAction - Next Location = ");
+    }
+}
