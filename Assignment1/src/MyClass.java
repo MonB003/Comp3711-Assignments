10a677e7227542406f5b79ae684392f3b08c9abb
Monica B <monicab3002@gmail.com>
2024-07-05 20:15:11 -0700

    Added environment array to store internal state for model agent. Updated code to use 2D array for indices

--- Assignment1/src/MyClass.java
+++ Assignment1/src/MyClass.java
@@ -2,20 +2,41 @@
  * MyClass is a class to handle the assignment 1 functionality of evaluating the next location of an action.
  */
 public class MyClass {
+    // Size stores how many rows and columns the environment has. For this assignment, there are 2 rows and 2 columns.
+    private static final int environmentSize = 2;
+    // Model based agent stores internal state of the environment
+    private static final boolean[][] agentEnvironment = new boolean[environmentSize][environmentSize];
+    // Stores the index positions of each letter
+    private static final char[][] letterIndices = {{'A', 'B'},{'C', 'D'}};
+
     /**
-     * Gets the index of a character.
-     * @param current_location: Character to find the index of.
-     * @return Integer value of the character's location index.
+     * Stores the command line arguments in the agentEnvironment array.
+     * @param A_status: Boolean value of square A's status
+     * @param B_status: Boolean value of square B's status
+     * @param C_status: Boolean value of square C's status
+     * @param D_status: Boolean value of square D's status
      */
-    public static int getLocationIndex(char current_location) {
-        int locationIndex = 0;
-        char[] letterIndices = {'A', 'B', 'C', 'D'};
-        // Start at 1 because boolean arguments start at 1
-        for (int index = 1; index <= letterIndices.length; ++index) {
-            char currentLetter = letterIndices[index-1];
-            if (current_location == currentLetter) {
-                // Set index
-                locationIndex = index;
+    public static void setupEnvironment(boolean A_status, boolean B_status, boolean C_status, boolean D_status) {
+        agentEnvironment[0][0] = A_status;
+        agentEnvironment[0][1] = B_status;
+        agentEnvironment[1][0] = C_status;
+        agentEnvironment[1][1] = D_status;
+    }
+
+    /**
+     * Gets the indices of a character.
+     * @param current_location: Character to find the index of.
+     * @return Integer array of the character's location indices.
+     */
+    public static int[] getLocationIndex(char current_location) {
+        int[] locationIndex = {0, 0};
+        for (int rowIndex = 0; rowIndex < letterIndices.length; ++rowIndex) {
+            for (int colIndex = 0; colIndex < letterIndices[rowIndex].length; ++colIndex) {
+                char currentLetter = letterIndices[rowIndex][colIndex];
+                if (current_location == currentLetter) {
+                    // Store the location's row and column indices
+                    locationIndex = new int[]{rowIndex, colIndex};
+                }
             }
         }
         return locationIndex;
@@ -23,57 +44,44 @@ public class MyClass {
 
     /**
      * Gets the character at an index.
-     * @param locationIndex: Integer value of the array index.
+     * @param rowIndex: Integer value of the array's row index.
+     * @param colIndex: Integer value of the array's column index.
      * @return Character at the index.
      */
-    public static char getLocationChar(int locationIndex) {
-        char[] letterIndices = {'A', 'B', 'C', 'D'};
-        // Array indices start at 0, so subtract 1 from the index value
-        return letterIndices[locationIndex-1];
+    public static char getLocationChar(int rowIndex, int colIndex) {
+        return letterIndices[rowIndex][colIndex];
     }
 
     /**
      * Gets the next square horizontal to the current location's square.
-     * @param locationIndex: Integer value of the array index.
-     * @return Integer value of the next location's index.
+     * @param rowIndex: Integer value of the array's row index.
+     * @param colIndex: Integer value of the array's column index.
+     * @return Integer array of the next location's indices.
      */
-    public static int getNextHorizontalSquare(int locationIndex) {
-        // If the number is even, move left. If it's odd, move right.
-        return (locationIndex % 2 == 0) ? (locationIndex - 1) : (locationIndex + 1);
+    public static int[] getNextHorizontalSquare(int rowIndex, int colIndex) {
+        // Move left or right to the next column, use modulus to wrap around for index values
+        return new int[]{rowIndex, (colIndex + 1) % environmentSize};
     }
 
     /**
      * Gets the next square vertical to the current location's square.
-     * @param locationIndex: Integer value of the array index.
-     * @return Integer value of the next location's index.
+     * @param rowIndex: Integer value of the array's row index.
+     * @param colIndex: Integer value of the array's column index.
+     * @return Integer array of the next location's indices.
      */
-    public static int getNextVerticalSquare(int locationIndex) {
-        // Row size is how many squares to move past before getting the next location index. For this assignment, there are 2 squares per row
-        int rowSize = 2;
-        // Table size is how many squares are in the table. For this assignment, there are 4 squares
-        int tableSize = rowSize * rowSize;
-        // If adding the row size this index is an index larger than the table, move up. If not, move down.
-        return (locationIndex + rowSize) > tableSize ? (locationIndex - rowSize) : (locationIndex + rowSize);
+    public static int[] getNextVerticalSquare(int rowIndex, int colIndex) {
+        // Move up or down to the next row, use modulus to wrap around for index values
+        return new int[]{(rowIndex + 1) % environmentSize, colIndex};
     }
 
     /**
      * Gets the next square diagonal to the current location's square.
-     * @param locationIndex: Integer value of the array index.
-     * @return Integer value of the next location's index.
+     * @param rowIndex: Integer value of the array's row index.
+     * @param colIndex: Integer value of the array's column index.
+     * @return Integer array of the next location's indices.
      */
-    public static int getNextDiagonalSquare(int locationIndex) {
-        switch (locationIndex) {
-            case 1:
-                return 4;
-            case 2:
-                return 3;
-            case 3:
-                return 2;
-            case 4:
-                return 1;
-            default:
-                return 0; // If error occurs
-        }
+    public static int[] getNextDiagonalSquare(int rowIndex, int colIndex) {
+        return new int[]{(rowIndex + 1) % environmentSize, (colIndex + 1) % environmentSize};
     }
 
     /**
@@ -81,7 +89,7 @@ public class MyClass {
      * @param args: Command line arguments passed to the program.
      */
     public static void main(String[] args) {
-        // Check arguments
+        // Check for invalid argument count
         if (args.length != 5) {
             System.out.println("Invalid number of command line arguments passed: " + args.length);
             return;
@@ -99,6 +107,9 @@ public class MyClass {
                 "Square C status = " + C_status + "\n" +
                 "Square D status = " + D_status + "\n");
 
+        // Store each location's status
+        setupEnvironment(A_status, B_status, C_status, D_status);
+
         // Assignment 1 function
         // Store next location, set a default value
         char nextLocation = current_location;
@@ -111,8 +122,8 @@ public class MyClass {
         }
 
         // Get current location info
-        int locationIndex = getLocationIndex(current_location);
-        boolean currentLocationStatus = Boolean.parseBoolean(args[locationIndex]);
+        int[] locationIndex = getLocationIndex(current_location);
+        boolean currentLocationStatus = agentEnvironment[locationIndex[0]][locationIndex[1]];
 
         // If the current location is not clean, the vacuum cleaner stays in its current location to clean it up.
         if (!currentLocationStatus) {
@@ -121,29 +132,29 @@ public class MyClass {
         }
 
         // Check next horizontal position. Horizontal moves have the highest priority over vertical moves
-        int nextLocationIndex = getNextHorizontalSquare(locationIndex);
-        boolean nextLocationStatus = Boolean.parseBoolean(args[nextLocationIndex]);
+        int[] nextLocationIndex = getNextHorizontalSquare(locationIndex[0], locationIndex[1]);
+        boolean nextLocationStatus = agentEnvironment[nextLocationIndex[0]][nextLocationIndex[1]];
         if (!nextLocationStatus) {
-            nextLocation = getLocationChar(nextLocationIndex);
+            nextLocation = getLocationChar(nextLocationIndex[0], nextLocationIndex[1]);
             System.out.println("\nAction - Next Location = " + nextLocation);
             return;
         }
 
         // If next horizontal position is already clean, check next vertical position
-        nextLocationIndex = getNextVerticalSquare(locationIndex);
-        nextLocationStatus = Boolean.parseBoolean(args[nextLocationIndex]);
+        nextLocationIndex = getNextVerticalSquare(locationIndex[0], locationIndex[1]);
+        nextLocationStatus = agentEnvironment[nextLocationIndex[0]][nextLocationIndex[1]];
         if (!nextLocationStatus) {
-            nextLocation = getLocationChar(nextLocationIndex);
+            nextLocation = getLocationChar(nextLocationIndex[0], nextLocationIndex[1]);
             System.out.println("\nAction - Next Location = " + nextLocation);
             return;
         }
 
         // If next vertical position is already clean, check next diagonal position
-        int diagonalLocationIndex = getNextDiagonalSquare(locationIndex);
-        boolean diagonalLocationStatus = Boolean.parseBoolean(args[diagonalLocationIndex]);
+        int[] diagonalLocationIndex = getNextDiagonalSquare(locationIndex[0], locationIndex[1]);
+        boolean diagonalLocationStatus = agentEnvironment[diagonalLocationIndex[0]][diagonalLocationIndex[1]];
         if (!diagonalLocationStatus) {
             // If diagonal is not clean, move vertically because vacuum cannot move diagonally
-            nextLocation = getLocationChar(nextLocationIndex);
+            nextLocation = getLocationChar(nextLocationIndex[0], nextLocationIndex[1]);
         }
         System.out.println("\nAction - Next Location = " + nextLocation);
     }
