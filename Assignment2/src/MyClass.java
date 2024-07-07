5af82359260a500f25f4c73eb67f6f4915cd2324
Monica B <monicab3002@gmail.com>
2024-07-06 22:02:32 -0700

    Used Java data structures to store nodes. Added generic loop to identify goal states. Added method to get all nodes in the path

--- Assignment2/src/MyClass.java
+++ Assignment2/src/MyClass.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class MyClass {
     public static String[] stateLetters = {"A", "B", "C", "D", "E", "H", "J", "G1", "G2", "G3"};
@@ -12,43 +12,83 @@ public class MyClass {
         }
         return false;
     }
+//    public static boolean isGoalState(int stateIndex, int[] heuristic_vector) {
+//        for (int index = 0; index < heuristic_vector.length; index++) {
+//            if ((index == stateIndex) && (heuristic_vector[index] == 0)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     public static int[] performAStarSearch(int[][] cost_matrix, int[] heuristic_vector, int startStateIndex, int[] goalStateIndices) {
-        // Check if the current state is a goal state
-        for (int goalState: goalStateIndices) {
-            if (goalState == startStateIndex) {
-                // Use this state?
-                return new int[]{startStateIndex, 0};
-            }
-        }
-
         // Store node indices to visit and their costs
-        ArrayList<int[]> openNodes = new ArrayList<>();
+        PriorityQueue<int[]> openNodes = new PriorityQueue<>(Comparator.comparingInt(node -> node[1]));
+        // Store visited nodes
+        HashSet<Integer> closedNodes = new HashSet<>();
+        // Store all previously visited states
+        HashMap<Integer, Integer> previousStates = new HashMap<>();
+        // Store g(n) function results as (state index, path cost) pairs
+        HashMap<Integer, Integer> initialPathCosts = new HashMap<>();
+        // Store f(n) function results as (state index, estimated cost) pairs
+        HashMap<Integer, Integer> estimatedCosts = new HashMap<>();
+
+        // Store start state values
+        initialPathCosts.put(startStateIndex, 0);
+        estimatedCosts.put(startStateIndex, heuristic_vector[startStateIndex]);
+        openNodes.add(new int[]{startStateIndex, estimatedCosts.get(startStateIndex)});
 
         // Check if there are still open nodes to explore
-        for (int index = 0; index < cost_matrix[startStateIndex].length; index++) {
-            // Store current path cost
-            int currentCost = cost_matrix[startStateIndex][index];
-            System.out.println("Current cost: " + currentCost);
+        while (!openNodes.isEmpty()) {
+            // Get node with the lowest estimated cost
+            int[] currentNode = openNodes.poll();
+            int currentState = currentNode[0];
 
-            // If cost is not 0, there's a path to the current node
-            if (currentCost > 0) {
-                // Calculate heuristic function: f(n) = g(n) + h(n)
-                int costSum = currentCost + heuristic_vector[index];
-                int[] indexCost = {index, costSum};
-                openNodes.add(indexCost);
+            // Check if the current state is a goal state
+            if (isGoalState(currentState, goalStateIndices)) {
+                return getPath(previousStates, currentState);
+            }
+
+            // State has been visited
+            closedNodes.add(currentState);
+
+            // Check neighbour nodes for the next state in the path
+            for (int neighbourIndex = 0; neighbourIndex < cost_matrix.length; neighbourIndex++) {
+                // Store current path cost
+                int currentPathCost = cost_matrix[currentState][neighbourIndex];
+                if (currentPathCost == 0 || closedNodes.contains(neighbourIndex)) {
+                    // Skip this neighbour node
+                    continue;
+                }
+
+                int gPathCost = initialPathCosts.getOrDefault(currentState, Integer.MAX_VALUE) + currentPathCost;
+                if (gPathCost < initialPathCosts.getOrDefault(neighbourIndex, Integer.MAX_VALUE)) {
+                    previousStates.put(neighbourIndex, currentState);
+                    initialPathCosts.put(neighbourIndex, gPathCost);
+                    // Calculate heuristic function: f(n) = g(n) + h(n) = path cost + estimated cost
+                    int estimateCostSum = gPathCost + heuristic_vector[neighbourIndex];
+                    estimatedCosts.put(neighbourIndex, estimateCostSum);
+                    openNodes.add(new int[]{neighbourIndex, initialPathCosts.get(neighbourIndex)});
+                }
             }
         }
 
-        // Find next start node state
-        int[] nextStartState = {0, 0};
-        for (int[] currentNode: openNodes) {
-            if (currentNode[1] > nextStartState[1]) {
-                nextStartState = currentNode;
-            }
+        // Return empty path if no path is found
+        return new int[0];
+    }
+
+    private static int[] getPath(HashMap<Integer, Integer> previousStates, int currentState) {
+        ArrayList<Integer> allPathStates = new ArrayList<>();
+        allPathStates.add(currentState);
+        while (previousStates.containsKey(currentState)) {
+            currentState = previousStates.get(currentState);
+            allPathStates.add(currentState);
         }
-        System.out.println("Next start state: " + stateLetters[nextStartState[0]]);
-        return nextStartState;
+        int[] path = new int[allPathStates.size()];
+        for (int index = 0; index < path.length; index++) {
+            path[index] = allPathStates.get(allPathStates.size() - 1 - index);
+        }
+        return path;
     }
 
     public static void main(String[] args) {
@@ -68,7 +108,18 @@ public class MyClass {
         int[] heuristic_vector = {5,7,3,4,6,8,5,0,0,0};
 
         // Identify goal states and save them in a new vector
-        int[] goalStateIndices = {7, 8, 9};
+        ArrayList<Integer> goalStatesList = new ArrayList<>();
+//        int[] goalStateIndices = {7, 8, 9}; // With hardcoded values
+        for (int index = 0; index < heuristic_vector.length; index++) {
+            // Goal states have a 0 value in the heuristic vector
+            if (heuristic_vector[index] == 0) {
+                goalStatesList.add(index);
+            }
+        }
+        int[] goalStateIndices = new int[goalStatesList.size()];
+        for (int goalIndex = 0; goalIndex < goalStatesList.size(); goalIndex++) {
+           goalStateIndices[goalIndex] = goalStatesList.get(goalIndex);
+        }
 
         // Write a program to implement the A * search
         // Print the cheapest path, the goal state and the number of cycles
@@ -76,11 +127,17 @@ public class MyClass {
         // Start state is A
         int startStateIndex = 0;
 
-        while (!isGoalState(startStateIndex, goalStateIndices)) {
-            int[] nextStartState = performAStarSearch(cost_matrix, heuristic_vector, startStateIndex, goalStateIndices);
-            startStateIndex = nextStartState[0];
+        // Perform A* search
+        int[] cheapestPath = performAStarSearch(cost_matrix, heuristic_vector, startStateIndex, goalStateIndices);
+        // Print results
+        if (cheapestPath.length > 0) {
+            System.out.println("Cheapest path: ");
+            for (int currentState: cheapestPath) {
+                System.out.print(stateLetters[currentState] + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("There was no path found to any goal state.");
         }
-
-        System.out.println("DONE WHILE LOOP");
     }
 }
